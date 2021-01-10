988be9dca7ec394acff4e4748afa53487b884654
Camilo <Camilo@Camilos-MacBook-Pro.local>
2021-01-10 15:36:55 -0500

    RestServer-usuario

--- server/config/config.js
+++ server/config/config.js
@@ -2,3 +2,23 @@
 // puerto
 //=====================
 process.env.PORT = process.env.PORT || 3000;
+
+//=====================
+// Entorno
+//=====================
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev'
+
+//=====================
+// Base de Datos
+//=====================
+
+let urlDB;
+
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe'
+} else {
+    urlDB = 'mongodb+srv://CamiloG:yNAVcbmlzyp212Pr@cluster0.yp8ay.mongodb.net/cafe'
+}
+
+process.env.URLDB = urlDB
