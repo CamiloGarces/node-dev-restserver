67c03c90f99adaa3c4d6b9fec78bf64420db1c1e
Camilo <Camilo@Camilos-MacBook-Pro.local>
2021-01-10 16:46:13 -0500

    environment variable

--- server/config/config.js
+++ server/config/config.js
@@ -18,7 +18,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe'
 } else {
-    urlDB = 'mongodb+srv://CamiloG:yNAVcbmlzyp212Pr@cluster0.yp8ay.mongodb.net/cafe'
+    urlDB = process.env.MONGO_URI
 }
 
 process.env.URLDB = urlDB
